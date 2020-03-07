066b181653ab6ffdd4fb339d8b2d61761f87f009
Carman Babin <CBabin@slb.com>
2020-03-06 18:22:02 -0600

    first build

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use clap::{App, AppSettings, SubCommand, Arg};
+use chrono::{NaiveTime, Utc, Timelike, Duration};
+use std::error::Error;
+use device_query::{DeviceState, Keycode, DeviceQuery};
+use std::{thread, io};
+use inputbot::KeybdKey::{NumLockKey};
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("away")
+        .version("0.1.0")
+        .about("Away management  tool")
+        .setting(AppSettings::ColorAlways)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::with_name("Duration")
+            .short("d")
+            .long("duration")
+            .help("example: 1:15:30 'hours:minutes:seconds'")
+            .takes_value(true))
+        .subcommand(SubCommand::with_name("i")
+            .about("Runs indefinitely"))
+        .get_matches();
+
+    if let Some(arg) = matches.value_of("Duration") {
+        run_duration(arg)?
+    }
+
+    if let Some(_) = matches.subcommand_matches("i") {
+        run_indefinitely();
+    }
+
+    Ok(())
+}
+
+fn run_duration(arg: &str) -> Result<(), Box<dyn Error>> {
+    let parsed_time: NaiveTime = match NaiveTime::parse_from_str(arg, "%H:%M:%S") {
+        Ok(res) => res,
+        Err(err) => {
+            eprintln!("Could not parse duration input. Example: 1:15:30");
+            return Err(Box::new(err));
+        }
+    };
+
+    let hour_dur = Duration::hours(parsed_time.hour() as i64);
+    let min_dur = Duration::minutes(parsed_time.minute() as i64);
+    let sec_dur = Duration::seconds(parsed_time.second() as i64);
+
+    let duration = hour_dur + min_dur + sec_dur;
+
+    let now = Utc::now();
+    let stop_time = now + duration;
+
+    loop {
+        let exited = check_for_exit_key();
+
+        let now = Utc::now();
+        let elapsed_time = stop_time - now;
+
+        print!("{}:{}:{}", elapsed_time.num_seconds() / 3600, (elapsed_time.num_seconds() / 60) % 60, elapsed_time.num_seconds() % 60);
+        io::stdout().flush().unwrap();
+
+        stay_awake();
+
+        print!("\r");
+        io::stdout().flush().unwrap();
+
+        if exited || now >= stop_time {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn run_indefinitely() {
+    loop {
+        let exited = check_for_exit_key();
+
+        stay_awake();
+
+        if exited {
+            break;
+        }
+    }
+    println!("Welcome back 🙂");
+}
+
+fn stay_awake() {
+    NumLockKey.press();
+    thread::sleep(std::time::Duration::from_millis(150));
+    NumLockKey.release();
+}
+
+fn check_for_exit_key() -> bool {
+    let keys: Vec<Keycode> = DeviceState.get_keys();
+    for key in keys.iter() {
+        return if *key == Keycode::Q {
+            true
+        } else {
+            false
+        }
+    }
+
+    return false;
+}
