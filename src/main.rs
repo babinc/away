85aeff5f9e6c14e840cf6b7825daf27999644df6
Carman Babin <CBabin@slb.com>
2022-02-11 15:10:45 -0600

    Detects user input If user input detect it does not do anything Waits 60 seconds to check for user input again If no input it will remain keeping the computer awake

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use chrono::{NaiveTime, Utc, Timelike, Duration, Local};
+mod app;
+
 use std::error::Error;
-use device_query::{DeviceState, Keycode, DeviceQuery};
-use std::{thread, io, env};
-use inputbot::KeybdKey::{ScrollLockKey};
-use std::io::Write;
+use std::{env};
+use crate::app::App;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -23,7 +22,8 @@ fn parse_arguments(args: &Vec<String>) -> Result<(), Box<dyn Error>> {
     if args.contains(&TIME_ARGUMENT.to_string()) == true {
         let index = args.iter().position(|r| r == &TIME_ARGUMENT.to_string()).unwrap();
         if args.len() > index + 1 {
-            run_till_time(&args)?
+            let mut app = App::new();
+            app.run_till_time(&args)?;
         }
         else {
             eprintln!("Error: missing time parameter");
@@ -34,7 +34,8 @@ fn parse_arguments(args: &Vec<String>) -> Result<(), Box<dyn Error>> {
     else if args.contains(&DURATION.to_string()) == true {
         let index = args.iter().position(|r| r == &DURATION.to_string()).unwrap();
         if args.len() > index + 1 {
-            run_duration(&args)?
+            let mut app = App::new();
+            app.run_duration(&args)?;
         }
         else {
             eprintln!("Error: missing duration parameter");
@@ -43,7 +44,8 @@ fn parse_arguments(args: &Vec<String>) -> Result<(), Box<dyn Error>> {
         }
     }
     else if args.contains(&INDEFINITELY.to_string()) == true {
-        run_indefinitely();
+        let mut app = App::new();
+        app.run_indefinitely();
     }
     else if args.contains(&HELP.to_string()) == true {
         display_usage();
@@ -58,108 +60,6 @@ fn parse_arguments(args: &Vec<String>) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-fn run_duration(arg: &Vec<String>) -> Result<(), Box<dyn Error>> {
-    let parsed_time: NaiveTime = match NaiveTime::parse_from_str(arg[2].as_str(), "%H:%M:%S") {
-        Ok(res) => res,
-        Err(err) => {
-            eprintln!("Could not parse duration input. Example: 1:15:30");
-            return Err(Box::new(err));
-        }
-    };
-
-    let hour_dur = Duration::hours(parsed_time.hour() as i64);
-    let min_dur = Duration::minutes(parsed_time.minute() as i64);
-    let sec_dur = Duration::seconds(parsed_time.second() as i64);
-
-    let duration = hour_dur + min_dur + sec_dur;
-
-    let now = Utc::now();
-    let stop_time = now + duration;
-
-    loop {
-        let exited = check_for_exit_key();
-
-        let now = Utc::now();
-        let elapsed_time = stop_time - now;
-
-        print!("{}:{}:{}", elapsed_time.num_seconds() / 3600, (elapsed_time.num_seconds() / 60) % 60, elapsed_time.num_seconds() % 60);
-        io::stdout().flush().unwrap();
-
-        stay_awake();
-
-        print!("\r");
-        io::stdout().flush().unwrap();
-
-        if exited || now >= stop_time {
-            break;
-        }
-    }
-
-    Ok(())
-}
-
-fn run_till_time(arg: &Vec<String>) -> Result<(), Box<dyn Error>> {
-    let parsed_time: NaiveTime = match NaiveTime::parse_from_str(arg[2].to_lowercase().as_str(), "%I:%M:%p") {
-        Ok(res) => res,
-        Err(err) => {
-            eprintln!("Could not parse time input. Example: 10:00:am");
-            return Err(Box::new(err));
-        }
-    };
-
-    loop {
-        let exited = check_for_exit_key();
-
-        let local_time = Local::now().time();
-        let elapsed_time = parsed_time - local_time;
-
-        print!("{}:{}:{}", elapsed_time.num_seconds() / 3600, (elapsed_time.num_seconds() / 60) % 60, elapsed_time.num_seconds() % 60);
-        io::stdout().flush().unwrap();
-
-        stay_awake();
-
-        print!("\r");
-        io::stdout().flush().unwrap();
-
-        if exited || local_time >= parsed_time.to_owned() {
-            break;
-        }
-    }
-
-    Ok(())
-}
-
-fn run_indefinitely() {
-    loop {
-        let exited = check_for_exit_key();
-
-        stay_awake();
-
-        if exited {
-            break;
-        }
-    }
-}
-
-fn stay_awake() {
-    ScrollLockKey.press();
-    thread::sleep(std::time::Duration::from_millis(150));
-    ScrollLockKey.release();
-}
-
-fn check_for_exit_key() -> bool {
-    let keys: Vec<Keycode> = DeviceState.get_keys();
-    for key in keys.iter() {
-        return if *key == Keycode::Q {
-            true
-        } else {
-            false
-        }
-    }
-
-    return false;
 }
 
 fn display_usage() {
