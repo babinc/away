4537c06bc82bf44e3d9a284da72d7a9c94a7c037
Carman Babin <carman.babin@gmail.com>
2022-02-15 09:09:20 -0600

    Clean up config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,8 @@
+use std::{env, fs};
+use std::error::Error;
+use std::fs::File;
+use std::io::{ErrorKind, Write};
+use std::path::Path;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -6,7 +11,7 @@ pub struct Config {
     pub key_press_time_ms: u64,
 }
 
-impl ::std::default::Default for Config {
+impl std::default::Default for Config {
     fn default() -> Self {
         Self {
             user_input_wait_time_ms: 60_000,
@@ -14,3 +19,72 @@ impl ::std::default::Default for Config {
         }
     }
 }
+
+impl Config {
+    pub fn load_config() -> Result<Config, Box<dyn Error>> {
+        let file_path = "config.json";
+
+        let mut config = Config::default();
+
+        let path = Path::new(file_path);
+
+        let exe_path = match env::current_exe() {
+            Ok(exe_path) => format!("{}", exe_path.display()),
+            Err(e) => {
+                let error_msg = format!("Error getting executable path: {}", e.to_string());
+                let err = std::io::Error::new(ErrorKind::Other, error_msg);
+                return Err(Box::new(err));
+            },
+        };
+
+        let full_path = format!("{}\\{}", exe_path, path.to_str().unwrap());
+
+        if path.exists() && path.is_file() {
+            let file_str = fs::read_to_string(file_path)?;
+
+            if file_str.len() > 0 {
+                match serde_json::from_str(file_str.as_ref()) {
+                    Ok(res) => config = res,
+                    Err(err) => {
+                        let error_msg = format!("Path: {}, Error parsing config.json: {}. Applying default values", path.to_str().unwrap(), err);
+                        eprintln!("{}", error_msg);
+                        fs::remove_file(file_path)?;
+                        return Ok(Config::default());
+                    }
+                };
+            }
+
+            println!("Using config file: {}", full_path);
+        }
+        else {
+            let mut file = match File::create(&file_path) {
+                Ok(res) => res,
+                Err(err) => {
+                    let error_msg = format!("Error: Trying parse new config.json: {}", err.to_string());
+                    let err = std::io::Error::new(ErrorKind::Other, error_msg);
+                    return Err(Box::new(err));
+                }
+            };
+
+            let json_data = serde_json::to_string_pretty(&config)?;
+
+            if json_data.len() > 0 {
+                match file.write_all(json_data.as_ref()) {
+                    Ok(_res) => {},
+                    Err(err) => {
+                        let error_msg = format!("Error trying to write new config.json: {}", err.to_string());
+                        let err = std::io::Error::new(ErrorKind::Other, error_msg);
+                        return Err(Box::new(err));
+                    }
+                }
+            }
+
+            println!("New config file created: {}", full_path);
+        }
+
+        println!("config values: {:?}", config);
+
+        Ok(config)
+    }
+}
+
